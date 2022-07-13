85e798524efe9aec8f5bea03b3cc152b5617ae4f
Nicholas Gill <mythagel@gmail.com>
2022-07-13 13:27:39 +1000

    test code

--- main.py
+++ main.py
@@ -1,26 +1,48 @@
+import time
 import machine
 import ntptime
+#import settings
 
-ww = machine.PWM(machine.Pin(0), freq=1000)
-cw = machine.PWM(machine.Pin(2), freq=1000)
+warmPwm = machine.PWM(machine.Pin(5), freq=1000)
+coolPwm = machine.PWM(machine.Pin(4), freq=1000)
 
-ww.duty(0)
-cw.duty(512)
-
-#    cold_white_color_temperature: 5700 K
-#    warm_white_color_temperature: 3000 K
+#ww.duty(0)
+#cw.duty(16)
 
 # TODO just have functions to write config files to set the timezone offsets / alarm time
 
-wwct = 3000
-cwct = 5700
+# 3000 - 4350 = 100% ww, 0 -> 100% cw
+# 4351 - 5700 = 100% cw, 100 -> 0% ww
+def clamp(x : float): return max(0.0, min(x, 1.0))
+def setColour(ct : int, br : float):
+    # ww = 3000, cw = 5700
+    ww = clamp(1 - (ct - 4351) / (5700 - 4351))
+    cw = clamp((ct - 3000) / (4350 - 3000))
+    br = clamp(br)
+#    print(ct, int(ww * 1023 * br), int(cw * 1023 * br))
 
-ct = lerp(wwct, cwct, 0.0)
-br = 0.0
-
-def lerp(a: float, b: float, t: float) -> float:
-    return (1 - t) * a + t * b
+    # TODO brightness probably isn't linear
+    warmPwm.duty(int(ww * 1023 * br))
+    coolPwm.duty(int(cw * 1023 * br))
 
 def localtime():
-    # TODO
-    pass
+    import time
+    now = time.time();
+
+    offset = 0;
+    for tz in settings.tzinfo:
+        if tz[0] > now:
+            break
+        offset = tz[1]
+    return time.localtime(now + offset)
+
+# Fade ct over an hour
+interval = 120.0 / (5700 - 3000)   # TODO 3600.0
+br_step = 1.0 / (5700 - 3000)
+br = 0.1
+for ct in range(3000, 5700):
+    setColour(ct, br);
+    br = br + br_step
+    time.sleep(interval)
+
+setColour(3000, 0.0);
