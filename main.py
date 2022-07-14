da902c7fd25e6022bf4f362018747506efd6b3aa
Nicholas Gill <mythagel@gmail.com>
2022-07-14 20:25:01 +1000

    Tidy

--- main.py
+++ main.py
@@ -27,8 +27,7 @@ def setColour(ct : int, br : float):
     # ww = 3000, cw = 5700
     ww = clamp(1 - (ct - 4351) / (5700 - 4351))
     cw = clamp((ct - 3000) / (4350 - 3000))
-    br = clamp(br)
-    # TODO brightness isn't linear
+    br = clamp(br*br)
     warmPwm.duty(int(ww * 1023 * br))
     coolPwm.duty(int(cw * 1023 * br))
 
@@ -38,7 +37,7 @@ def sunriseSequence(duration : float):
     br_step = 1.0 / (5700 - 3000)
     br = 0
     for ct in range(3000, 5700):
-        setColour(ct, br*br)
+        setColour(ct, br)
         br += br_step
         time.sleep(interval)
     setColour(5700, 1.0)
